09f1c05e76adc4267f1506bf6b5a49b4add356ed
liv3rise <liv3rise@gmail.com>
2023-01-16 11:33:51 +0200

    theme switch testing

--- public/theme-switch.js
+++ public/theme-switch.js
@@ -1,27 +1,46 @@
-const themeToggler = document.getElementById("themeToggler");
-
-console.log(localStorage.getItem('darkTheme'));
-
 localStorage.getItem('darkTheme') === 'true' ? switchTheme(true) : switchTheme(false);
 
-function switchTheme(darkTheme) {
+function switchTheme(darkTheme, toggler) {
   if (darkTheme) {
     console.log('switching to dark');
     document.documentElement.classList.add("dark");
-    themeToggler.classList.add("switch_active");
     localStorage.setItem('darkTheme', darkTheme);
   } else {
     console.log('switching to light');
     document.documentElement.classList.remove("dark");
-    themeToggler.classList.remove("switch_active");
     localStorage.setItem('darkTheme', darkTheme);
   }
+  toggler && togglerClassHandler(toggler, darkTheme);
 }
 
 function isDark() {
   return document.documentElement.classList.contains("dark");
 }
 
-themeToggler.addEventListener("click", (e) => {
-  switchTheme(!isDark());
-});
+function togglerClassHandler(toggler, isDark) {
+  if (isDark) {
+    toggler.classList.add('switch_active');
+  } else {
+    toggler.classList.remove('switch_active');
+  }
+}
+
+window.onload = () => {
+  const themeToggler = document.getElementById("themeToggler");
+
+  togglerClassHandler(themeToggler, isDark());
+
+  themeToggler.addEventListener("click", (e) => {
+    switchTheme(!isDark(), themeToggler);
+  });
+}
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const themeToggler = document.getElementById("themeToggler");
+
+//   togglerClassHandler(themeToggler, isDark());
+
+//   themeToggler.addEventListener("click", (e) => {
+//     switchTheme(!isDark(), themeToggler);
+//   });
+// });
